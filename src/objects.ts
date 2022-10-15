7c2a807d7c341a279a67460fd55837a5c93a277d
Joost Lubach <joostlubach@gmail.com>
2022-10-15 14:56:31 +0200

    Adding back both versions.

--- src/objects.ts
+++ src/objects.ts
@@ -1,4 +1,7 @@
-import { isArray, isEqual, isPlainObject } from 'lodash'
+import { isArray, isEqual, isObject, isPlainObject } from 'lodash'
+
+//------
+// objectEquals
 
 export function objectEquals(left: AnyObject | null | undefined, right: AnyObject | null | undefined, equals: (a: any, b: any) => boolean = isEqual): boolean {
   if (left == null) { return right == null }
@@ -15,10 +18,13 @@ export function objectEquals(left: AnyObject | null | undefined, right: AnyObjec
   return true
 }
 
-export function modifyInObject<T extends AnyObject>(root: T, path: string, modifier: (value: any) => any): T
-export function modifyInObject<T extends AnyObject>(root: T[], path: string, modifier: (value: any) => any): T[]
-export function modifyInObject<T extends AnyObject>(root: T | T[], path: string, modifier: (value: any) => any): T | T[]
-export function modifyInObject(root: AnyObject | AnyObject[], path: string, modifier: (value: any) => any) {
+//------
+// modifyObject
+
+export function modifyObject<T extends AnyObject>(root: T, path: string, modifier: (value: any) => any): T
+export function modifyObject<T extends AnyObject>(root: T[], path: string, modifier: (value: any) => any): T[]
+export function modifyObject<T extends AnyObject>(root: T | T[], path: string, modifier: (value: any) => any): T | T[]
+export function modifyObject(root: AnyObject | AnyObject[], path: string, modifier: (value: any) => any) {
   const segments = path.split('.').filter(Boolean)
   return modify(root, segments, modifier)
 }
@@ -48,5 +54,74 @@ function modify(obj: any, segments: string[], modifier: (value: any) => any): an
   } else {
     // The property did not exist. Just return the object (instead of inserting an explicit `undefined`).
     return obj
+  }
+}
+
+//------
+// modifyInObject
+
+export function modifyInObject<T extends AnyObject>(root: T, path: string, modify: ModifyInObjectCallback) {
+  const segments = path.split('.')
+  const leaf     = segments.pop()!
+
+  let current: any = root
+  while (current != null && segments.length > 0) {
+    const segment = segments.shift()!
+    if (isArray(current)) {
+      current = current[keyToIndex(segment)]
+    } else if (isObject(current)) {
+      current = (current as any)[segment]
+    } else {
+      current = null
+    }
+  }
+  if (current == null) { return false }
+
+  const retval = modify(root, current, leaf)
+  return retval === false ? false : true
+}
+
+function keyToIndex(key: string) {
+  const index = parseInt(key, 10)
+  if (isNaN(index)) {
+    throw new Error("Cannot descend into array with a string key")
+  }
+  return index
+}
+
+export type ModifyInObjectCallback = <T extends AnyObject>(
+  root:   T,
+  parent: any,
+  key:    string | number
+) => void | boolean
+
+//------
+// deepMap*
+
+export function deepMapKeys(arg: any, fn: (key: string | symbol) => any): any {
+  if (isPlainObject(arg)) {
+    const result: Record<string, any> = {}
+    for (const [attribute, value] of Object.entries(arg)) {
+      result[fn(attribute)] = deepMapKeys(value, fn)
+    }
+    return result
+  } else if (isArray(arg)) {
+    return arg.map(it => deepMapKeys(it, fn))
+  } else {
+    return arg
+  }
+}
+
+export function deepMapValues(arg: any, fn: (value: any) => any): any {
+  if (isPlainObject(arg)) {
+    const result: Record<string, any> = {}
+    for (const [attribute, value] of Object.entries(arg)) {
+      result[attribute] = deepMapValues(value, fn)
+    }
+    return result
+  } else if (isArray(arg)) {
+    return arg.map(it => deepMapValues(it, fn))
+  } else {
+    return fn(arg)
   }
 }
