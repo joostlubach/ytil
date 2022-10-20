55021c2f3a1b5bdad6c76f320bfdda2c6c3e53e7
Joost Lubach <joostlubach@gmail.com>
2022-10-20 09:11:57 +0200

    Added EnumUtil.values().

--- src/EnumUtil.ts
+++ src/EnumUtil.ts
@@ -1,7 +1,11 @@
 export abstract class EnumUtil {
 
-  public static names(enumType: Record<any, any>) {
+  public static names<V>(enumType: Record<string, V>): string[] {
     return Object.keys(enumType)
+  }
+
+  public static values<V>(enumType: Record<string, V>): V[] {
+    return Object.values(enumType)
   }
 
 }
