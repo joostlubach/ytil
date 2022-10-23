e7a2214e03a074d4aeeb47258f5000c656c962ab
Joost Lubach <joostlubach@gmail.com>
2022-10-23 15:02:44 +0200

    Upgrade in MapBuilder.

--- src/MapBuilder.ts
+++ src/MapBuilder.ts
@@ -2,27 +2,33 @@ import { isArray } from 'lodash'
 
 export abstract class MapBuilder {
 
-  public static by<K, V>(values: V[], keyForValue: (value: V) => K): Map<K, V> {
+  public static by<It, K>(items: It[], keyForItem: (item: It) => K): Map<K, It>
+  public static by<It, K, V>(items: It[], keyForItem: (item: It) => K, valueForItem: (value: It) => V): Map<K, V>
+  public static by<It, K, V = It>(items: It[], keyForItem: (item: It) => K, valueForItem?: (value: It) => V): Map<K, V> {
     const result = new Map<K, V>()
-    for (const value of values) {
-      const key = keyForValue(value)
+    for (const item of items) {
+      const key   = keyForItem(item)
+      const value = valueForItem == null ? (item as any as V) : valueForItem(item)
       result.set(key, value)
     }
     return result
   }
 
-  public static groupBy<K, V>(values: V[], keyForValue: (value: V) => K | K[]): Map<K, V[]> {
+  public static groupBy<It, K>(items: It[], keyForItem: (item: It) => K): Map<K, It[]>
+  public static groupBy<It, K, V>(items: It[], keyForItem: (item: It) => K, valueForItem: (value: It) => V): Map<K, V[]>
+  public static groupBy<It, K, V>(items: It[], keyForItem: (item: It) => K | K[], valueForItem?: (value: It) => V): Map<K, V[]> {
     const result = new Map<K, V[]>()
 
-    for (const value of values) {
-      const key = keyForValue(value)
-      const keys = isArray(key) ? key : [key]
+    for (const item of items) {
+      const key   = keyForItem(item)
+      const keys  = isArray(key) ? key : [key]
+      const value = valueForItem == null ? (item as any as V) : valueForItem(item)
+
       for (const key of keys) {
-        let item = result.get(key)
-        if (item == null) {
-          result.set(key, item = [])
-        }
-        item.push(value)
+        const resultItem = result.get(key) ?? []
+        result.set(key, resultItem)
+
+        resultItem.push(value)
       }
     }
 
