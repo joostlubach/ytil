859c029f2ea0296fbdfc2c00fc6262738c0b5d87
Joost Lubach <joostlubach@gmail.com>
2022-11-05 18:13:44 +0100

    Formatting.

---
+++ src/formatting.ts
@@ -0,0 +1,8 @@
+export function formatList(list: any[], headSeparator: string = ', ', tailSeparator: string = ' and ') {
+  if (list.length === 0) { return ''}
+  if (list.length === 1) { return `${list[0]}` }
+
+  const head = [...list]
+  const tail = head.pop()
+  return [head.join(headSeparator), tail].join(tailSeparator)
+}
