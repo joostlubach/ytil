d9d9e4317b2cb86362ab321335ff50553ff65d63
Joost Lubach <joostlubach@gmail.com>
2022-11-12 14:46:59 +0100

    Added SetOperations.intersection.

--- src/SetOperations.ts
+++ src/SetOperations.ts
@@ -6,4 +6,8 @@ export abstract class SetOperations {
     return arr1.filter(it1 => !some(arr2, it2 => equals(it1, it2)))
   }
 
+  public static intersection<T>(arr1: T[], arr2: T[], equals: ((left: T, right: T) => boolean) = isEqual): T[] {
+    return arr1.filter(it1 => some(arr2, it2 => equals(it1, it2)))
+  }
+
 }
