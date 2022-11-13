4e7a4dbbc70bd20685e6b01534158989375d6c6c
Joost Lubach <joostlubach@gmail.com>
2022-11-13 15:16:29 +0100

    Fix in EnumUtil.

--- src/EnumUtil.ts
+++ src/EnumUtil.ts
@@ -1,13 +1,27 @@
+import { every } from 'lodash'
+
 export abstract class EnumUtil {
 
   public static names<V>(enumType: Record<string, V>): string[] {
-    const count = Object.keys(enumType).length
-    return Object.values(enumType).slice(0, count / 2) as string[]
+    if (this.isIdentityMap(enumType)) {
+      return Object.keys(enumType)
+    } else {
+      const count = Object.keys(enumType).length
+      return Object.values(enumType).slice(0, count / 2) as string[]
+    }
   }
 
   public static values<V>(enumType: Record<string, V>): V[] {
-    const count = Object.keys(enumType).length
-    return Object.values(enumType).slice(count / 2) as V[]
+    if (this.isIdentityMap(enumType)) {
+      return Object.values(enumType)
+    } else {
+      const count = Object.keys(enumType).length
+      return Object.values(enumType).slice(count / 2) as V[]
+    }
+  }
+
+  public static isIdentityMap(enumType: Record<string, any>) {
+    return every(Object.entries(enumType), ([key, value]) => key === value)
   }
 
 }
