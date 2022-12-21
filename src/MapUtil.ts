d64451d0a20b0b97e54cf1fede1cbcefd0e71ea4
Joost Lubach <joostlubach@gmail.com>
2022-12-21 16:35:59 +0100

    Added MapUtil.ensure.

--- src/MapUtil.ts
+++ src/MapUtil.ts
@@ -1,17 +1,27 @@
-export const MapUtil = {
+export abstract class MapUtil {
 
-  sparse<K extends PropertyKey, V>(map: Map<K, V | null | undefined>): Map<K, V> {
+  public static sparse<K extends PropertyKey, V>(map: Map<K, V | null | undefined>): Map<K, V> {
     const result = new Map<K, V>()
     for (const [key, value] of map.entries()) {
       if (value == null) { continue }
       result.set(key, value)
     }
     return result
-  },
+  }
 
-  mapToObject<K extends PropertyKey, V>(map: Map<K, V>): Record<K, V> {
+  public static mapToObject<K extends PropertyKey, V>(map: Map<K, V>): Record<K, V> {
     return Object.fromEntries(map.entries()) as Record<K, V>
-  },
+  }
 
+  public static ensure<K extends PropertyKey, V>(map: Map<K, V>, key: K, defaultValue: () => V): V
+  public static ensure<K extends object, V>(map: WeakMap<K, V>, key: K, defaultValue: () => V): V
+  public static ensure(map: Map<any, any> | WeakMap<any, any>, key: any, defaultValue: () => any) {
+    const value = map.get(key)
+    if (value !== undefined) { return value }
+
+    const newValue = defaultValue()
+    map.set(key, newValue)
+    return newValue
+  }
 
 }
