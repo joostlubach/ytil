e14a35bafe07582b2f790d567c000d81559b0d8b
Joost Lubach <joostlubach@gmail.com>
2023-01-25 13:19:57 +0100

    Added cartesian function.

--- src/arrays.ts
+++ src/arrays.ts
@@ -68,3 +68,38 @@ export function flatMap<T, U>(input: T[], fn: (value: T) => U | U[]): U[] {
   }
   return result
 }
+
+/**
+ * Given M arrays with N items, returns a new array which contains all combinations of the
+ * input array.
+ *
+ * Examples:
+ *   - `cartesian([1, 2], ['A', 'B']) => [[1, 'A'], [1, 'B'], [2, 'A'], [2, 'B']]`
+ *   - `cartesian([1, 2], [3, 4])     => [[1, 3], [1, 4], [2, 3], [2, 4]]`
+ *
+ * @param arrays The input arrays.
+ * @returns The cartesian product of all arrays.
+ */
+export function cartesian<T>(...arrays: T[][]): T[][] {
+  const results: T[][] = []
+
+  for (const array of arrays) {
+    if (array.length === 0) { continue }
+
+    if (results.length === 0) {
+      for (const item of array) {
+        results.push([item])
+      }
+    } else {
+      const next: T[][] = []
+      for (const result of results) {
+        for (const item of array) {
+          next.push([...result, item])
+        }
+      }
+      results.splice(0, results.length, ...next)
+    }
+  }
+
+  return results
+}
