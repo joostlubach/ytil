8acdd68ccd31b091b3a210a7835cc4a8ede190fd
Joost Lubach <joostlubach@gmail.com>
2023-07-17 13:32:42 +0200

    Prepping for @ctrl

--- src/numbers.ts
+++ src/numbers.ts
@@ -1,8 +1,12 @@
+export function safeParseFloat(arg: any, defaultValue: number): number
+export function safeParseFloat(arg: any, defaultValue?: null): number | null
 export function safeParseFloat(arg: any, defaultValue: number | null = null) {
   const number = parseFloat(arg)
   return isNaN(number) ? defaultValue : number
 }
 
+export function safeParseInt(arg: any, defaultValue: number, radix?: number): number
+export function safeParseInt(arg: any, defaultValue?: null, radix?: number): number | null
 export function safeParseInt(arg: any, defaultValue: number | null = null, radix: number = 10) {
   const number = parseInt(arg, radix)
   return isNaN(number) ? defaultValue : number
