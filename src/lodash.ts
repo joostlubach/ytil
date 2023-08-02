70d495ce00bbf5332280a795ac9b2583db0771b8
Joost Lubach <joostlubach@gmail.com>
2023-08-02 14:47:50 +0200

    Added more lodash shims.

--- src/lodash.ts
+++ src/lodash.ts
@@ -1,7 +1,8 @@
-import { isPlainObject as _isPlainObject } from 'lodash'
+import { isPlainObject as _isPlainObject, keys as _keys } from 'lodash'
 
 /**
  * This file contains some Lodash methods with slightly stricter typing.
  */
 
 export const isPlainObject = _isPlainObject as (obj: any) => obj is Record<string, any>
+export const objectKeys    = _keys as <T>(obj: T) => Array<keyof T>
