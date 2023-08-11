a6a69dcb21123a8802a2719bb1815a60f31f67b0
Joost Lubach <joostlubach@gmail.com>
2023-08-11 13:16:31 +0200

    Fixing build system for TS

--- src/lodashext.ts
+++ src/lodashext.ts
@@ -1,4 +1,4 @@
-import { isPlainObject as _isPlainObject, keys as _keys } from 'lodash'
+import { entries as _entries, isPlainObject as _isPlainObject, keys as _keys } from 'lodash'
 
 /**
  * This file contains some Lodash methods with slightly stricter typing.
@@ -6,3 +6,4 @@ import { isPlainObject as _isPlainObject, keys as _keys } from 'lodash'
 
 export const isPlainObject = _isPlainObject as (obj: any) => obj is Record<string, any>
 export const objectKeys    = _keys as <T>(obj: T) => Array<keyof T>
+export const objectEntries = _entries as <T>(obj: T) => Array<[keyof T, T[keyof T]]>
