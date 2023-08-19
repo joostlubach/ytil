e3b5f18052e9762ac077f8cc0e86b758e1e39b5a
Joost Lubach <joostlubach@gmail.com>
2023-08-19 10:25:48 +0200

    Adding more typed lodash extensions.

--- src/lodashext.ts
+++ src/lodashext.ts
@@ -1,4 +1,9 @@
-import { entries as _entries, isPlainObject as _isPlainObject, keys as _keys } from 'lodash'
+import {
+  entries as _entries,
+  isPlainObject as _isPlainObject,
+  keys as _keys,
+  values as _values,
+} from 'lodash'
 
 /**
  * This file contains some Lodash methods with slightly stricter typing.
@@ -6,4 +11,5 @@ import { entries as _entries, isPlainObject as _isPlainObject, keys as _keys } f
 
 export const isPlainObject = _isPlainObject as (obj: any) => obj is Record<string, any>
 export const objectKeys    = _keys as <T>(obj: T) => Array<keyof T>
+export const objectValues  = _values as <T>(obj: T) => Array<T[keyof T]>
 export const objectEntries = _entries as <T>(obj: T) => Array<[keyof T, T[keyof T]]>
