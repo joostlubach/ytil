724ba58b9a657f149cfe44cd50a16f14501204c1
Joost Lubach <joostlubach@gmail.com>
2023-09-06 13:46:42 +0200

    Added typed decorator types.

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,13 @@
+export type AnyFunction = (...args: any[]) => any
 export type Constructor<T> = new (...args: any[]) => T
 export type AnyConstructor = Constructor<any>
+
+
 export type InstanceOf<C> = C extends Constructor<infer T> ? T : never
 export type DeepPartial<T> = {
   [K in keyof T]?: T[K] extends Record<any, any> ? DeepPartial<T[K]> : T[K]
 }
+
+export type TypedClassDecorator<T extends AnyConstructor> = (target: T) => T | void
+export type TypedPropertyDecorator<T> = (target: Object, key: string | symbol, descriptor: TypedPropertyDescriptor<T>) => TypedPropertyDescriptor<T> | void
+export type TypedMethodDecorator<T extends AnyFunction> = (target: Object, key: string | symbol, descriptor: TypedPropertyDescriptor<T>) => TypedPropertyDescriptor<T> | void
