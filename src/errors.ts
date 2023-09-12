33ca7a09d9d9b2192d88e5a65a31e4d4609a13a6
Joost Lubach <joostlubach@gmail.com>
2023-09-12 11:24:27 +0200

    Guess I have to use NodeNext now

--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,6 @@
 import 'error-polyfill'
 
-export function panic(message: string) {
+export function panic(message: string): never {
   const error = new Error(message)
   Error.captureStackTrace(error, panic)
   throw error
