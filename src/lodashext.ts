950ef545932ca3e7530a59ca9f974dd25ed07287
Joost Lubach <joostlubach@gmail.com>
2024-04-04 12:38:07 +0200

    Fixes

--- src/lodashext.ts
+++ src/lodashext.ts
@@ -13,7 +13,7 @@ import { UnknownObject } from './types'
  */
 
 export const isPlainObject = _isPlainObject as <T extends UnknownObject>(obj: unknown) => obj is T
-export const isObject = _isObject as <T>(obj: unknown) => obj is T
+export const isObject = _isObject as <T extends object>(obj: unknown) => obj is T
 export const objectKeys = _keys as <T>(obj: T) => Array<keyof T>
 export const objectValues = _values as <T>(obj: T) => Array<T[keyof T]>
 export const objectEntries = _entries as <T>(obj: T) => Array<[keyof T, T[keyof T]]>
