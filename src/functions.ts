f44ff654804ef91863220590b654db6b1668eef9
Joost Lubach <joostlubach@gmail.com>
2024-07-06 14:26:48 +0200

    Added hasFunction.

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,12 @@
-export function isFunction<F extends (...args: any[]) => any>(fn: any): fn is F {
+import { AnyFunction } from './types'
+
+export function isFunction<F extends(...args: any[]) => any>(fn: any): fn is F {
   return typeof fn === 'function'
+}
+
+export function hasFunction<O extends object, K extends string | number | symbol>(obj: O, key: K): obj is O & {[key in K]: AnyFunction} {
+  if (!(key in obj)) { return false }
+  if (!isFunction((obj as any)[key])) { return false }
+
+  return true
 }
