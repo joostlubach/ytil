fa92e66dbaf772372f017ee89dd77b9c37830f00
Joost Lubach <joostlubach@gmail.com>
2024-09-13 12:37:23 +0200

    Added more set operations.

--- src/sets.ts
+++ src/sets.ts
@@ -1,9 +1,32 @@
 import { isEqual } from 'lodash'
 
-export function diff<T>(arr1: ArrayLike<T>, arr2: ArrayLike<T>, equals: ((left: T, right: T) => boolean) = isEqual): T[] {
-  return Array.from(arr1).filter(it1 => Array.from(arr2).some(it2 => equals(it1, it2)))
+export function diff<T>(lists: ArrayLike<T>[], options: SetOperationOptions<T> = {}): T[] {
+  const {equals = isEqual} = options
+  const arrays = lists.map(it => Array.from(it))
+  return arrays[0].filter(it1 => !arrays.slice(1).some(arr => arr.some(it2 => equals(it1, it2))))
 }
 
-export function intersect<T>(arr1: ArrayLike<T>, arr2: ArrayLike<T>, equals: ((left: T, right: T) => boolean) = isEqual): T[] {
-  return Array.from(arr1).filter(it1 => Array.from(arr2).some(it2 => equals(it1, it2)))
+export function intersect<T>(lists: ArrayLike<T>[], options: SetOperationOptions<T> = {}): T[] {
+  const {equals = isEqual} = options
+  const arrays = lists.map(it => Array.from(it))
+  return arrays[0].filter(it1 => arrays.slice(1).every(arr => arr.some(it2 => equals(it1, it2))))
+}
+
+export function union<T>(lists: ArrayLike<T>[], options: SetOperationOptions<T> = {}): T[] {
+  const {equals = isEqual} = options
+
+  const result: T[] = []
+  for (const list of lists) {
+    for (const value of Array.from(list)) {
+      if (!result.some(it => equals(it, value))) {
+        result.push(value)
+      }
+    }
+  }
+
+  return result
+}
+
+export interface SetOperationOptions<T> {
+  equals?: (left: T, right: T) => boolean
 }
