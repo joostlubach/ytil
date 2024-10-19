6f80e29ad1d424f829f2cfc6ee53b3a1f77123ef
Joost Lubach <joostlubach@gmail.com>
2024-10-19 13:20:16 +0200

    Camelize.

--- src/types.ts
+++ src/types.ts
@@ -6,10 +6,18 @@ export type AnyConstructor = Constructor<unknown>
 export type DeepPartial<T> = {
   [K in keyof T]?: T[K] extends Record<any, any> ? DeepPartial<T[K]> : T[K]
 }
-export type SomePartial<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
 
 export type TypedClassDecorator<T extends AnyConstructor> = (target: T) => T | void
 export type TypedMethodDecorator<T extends AnyFunction> = (target: object, key: string | symbol, descriptor: TypedPropertyDescriptor<T>) => TypedPropertyDescriptor<T> | void
+
+export type CamelizeKeys<T> = {
+  [K in keyof T as K extends string ? Camelize<K> : K]: T[K]
+}
+
+type Camelize<S extends string> = S extends `${infer Head}${'_' | '-' | ' '}${infer Tail}`
+  ? `${Head}${Capitalize<Camelize<Tail>>}`
+  : S
 
 export type UnknownObject<K extends ObjectKey = ObjectKey> = Record<K, unknown>
 export type EmptyObject<K extends ObjectKey = ObjectKey> = Record<K, never>
