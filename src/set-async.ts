8f3869897b97214b8483bb08626b60329f911de6
Joost Lubach <joostlubach@gmail.com>
2024-11-28 21:04:32 +0100

    Lint.

--- src/set-async.ts
+++ src/set-async.ts
@@ -1,7 +1,7 @@
 export async function filterAsync<T>(array: T[], predicate: (value: T) => Promise<any>): Promise<T[]> {
   const promises = array.map(predicate)
   const results = await Promise.all(promises)
-  return array.filter((item, index) => !!results[index])
+  return array.filter((_item, index) => !!results[index])
 }
 
 export async function mapAsync<T>(array: T[], predicate: (value: T) => Promise<any>): Promise<T[]> {
