c97708ab122c04abeb7a2994d67a2386b49f2d7c
Joost Lubach <joostlubach@gmail.com>
2024-12-13 16:26:50 +0100

    Fix in @memoized.

--- src/decorators/memoized.ts
+++ src/decorators/memoized.ts
@@ -3,17 +3,42 @@ import { isFunction } from '../functions'
 /**
  * Memoizes the return value of a function or property getter.
  */
-export function memoized(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-  const original = descriptor.get ?? descriptor.value
-  if (!isFunction(original)) {
+export function memoized(target: object, propertyKey: string, descriptor: PropertyDescriptor) {
+  if ('get' in descriptor) {
+    return memoizedProperty(propertyKey, descriptor)
+  } else if ('value' in descriptor && isFunction(descriptor.value)) {
+    return memoizedMethod(propertyKey, descriptor)
+  } else {
     throw new Error('@memoized can only be applied to methods or property getters')
   }
+}
 
+function memoizedProperty(propertyKey: string, descriptor: PropertyDescriptor) {
   const cacheKey = Symbol(`${propertyKey}(memoized)`)
-  descriptor.value = function(this: any, ...args: any[]) {
+
+  const original = descriptor.get
+  if (original == null) { return descriptor }
+
+  descriptor.get = function(this: any) {
     if (!this.hasOwnProperty(cacheKey)) {
-      this[cacheKey] = original.apply(this, args)
+      this[cacheKey] = original.call(this)
     }
     return this[cacheKey]
   }
+  return descriptor
+}
+
+function memoizedMethod(propertyKey: string, descriptor: PropertyDescriptor) {
+  const cacheKey = Symbol(`${propertyKey}(memoized)`)
+
+  const original = descriptor.value
+  if (original == null) { return descriptor }
+
+  descriptor.value = function(this: any, ...args: any[]) {
+    if (!this.hasOwnProperty(cacheKey)) {
+      this[cacheKey] = original.call(this, ...args)
+    }
+    return this[cacheKey]
+  }
+  return descriptor
 }
