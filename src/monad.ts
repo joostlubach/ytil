d9d7e2050514237890d69a12c08da1d7610c4d4f
Joost Lubach <joostlubach@gmail.com>
2025-10-01 11:01:58 +0200

    Fine tuning monad.map typings better

--- src/monad.ts
+++ src/monad.ts
@@ -3,7 +3,7 @@ import { isArray } from 'lodash'
 export namespace monad {
 
   export function map<T, U>(value: null, map: (value: T) => U): null
-  export function map<T, U>(value: T[], map: (value: T) => U): U[]
+  export function map<T, U>(value: T[] | T, map: (value: T) => U): U[]
   export function map<T, U>(value: T, map: (value: T) => U): U | U[]
   export function map<T, U>(value: T | T[] | null, map: (value: T) => U): U | U[] | null
   export function map<T, U>(value: T | T[], map: (value: T) => U): U | U[] | null{
