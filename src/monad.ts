5b54edd46523c6a26f53821cb627c9d1becdc0fd
Joost Lubach <joostlubach@gmail.com>
2025-10-01 11:01:15 +0200

    Fix

--- src/monad.ts
+++ src/monad.ts
@@ -2,22 +2,17 @@ import { isArray } from 'lodash'
 
 export namespace monad {
 
-  export function wrap<T>(value: T | T[]): T[] {
-    if (isArray(value)) {
-      return value
-    } else {
-      return [value]
-    }
-  }
-
+  export function map<T, U>(value: null, map: (value: T) => U): null
   export function map<T, U>(value: T[], map: (value: T) => U): U[]
-  export function map<T, U>(value: T | T[], map: (value: T) => U): U | U[]
-  export function map<T, U>(value: T, map: (value: T) => U): U
-  export function map<T, U>(value: T | T[], map: (value: T) => U): U | U[] {
+  export function map<T, U>(value: T, map: (value: T) => U): U | U[]
+  export function map<T, U>(value: T | T[] | null, map: (value: T) => U): U | U[] | null
+  export function map<T, U>(value: T | T[], map: (value: T) => U): U | U[] | null{
     if (isArray(value)) {
       return value.map(map)
-    } else {
+    } else if (value != null) {
       return map(value)
+    } else {
+      return null
     }
   }
 
