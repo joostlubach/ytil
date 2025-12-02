84a4f0ad06c8ce9772fa8abaf602fede5d4ef93f
Joost Lubach <joostlubach@gmail.com>
2025-12-02 14:48:57 +0100

    Support for both experimental and modern decorator syntax

--- src/decorators/memoized.ts
+++ src/decorators/memoized.ts
@@ -2,67 +2,96 @@
 
 /**
  * Memoizes the return value of a function or property getter.
+ * Supports both experimental and modern decorator syntax.
  */
-export function memoized(target: any, context: ClassMethodDecoratorContext | ClassGetterDecoratorContext) {
-  if (context.kind === 'getter') {
-    return function (this: any) {
-      const value = target.call(this)
-      Object.defineProperty(this, context.name, {
-        value,
-        writable:     false,
-        enumerable:   true,
-        configurable: true,
-      })
-      return value
+export function memoized<T, V>(
+  target: (this: T) => V,
+  context: ClassGetterDecoratorContext<T, V>
+): (this: T) => V
+
+export function memoized<T, A extends any[], V>(
+  target: (this: T, ...args: A) => V,
+  context: ClassMethodDecoratorContext<T, (this: T, ...args: A) => V>
+): (this: T, ...args: A) => V
+
+export function memoized<T>(
+  target: any,
+  propertyKey: string | symbol,
+  descriptor: PropertyDescriptor
+): PropertyDescriptor
+
+export function memoized(target: any, context?: any, descriptor?: PropertyDescriptor): any {
+  // Modern decorator syntax (context is an object with 'kind' property)
+  if (context != null && typeof context === 'object' && 'kind' in context) {
+    if (context.kind === 'getter') {
+      return function (this: any) {
+        const value = target.call(this)
+        Object.defineProperty(this, context.name, {
+          value,
+          writable:     false,
+          enumerable:   true,
+          configurable: true,
+        })
+        return value
+      }
+    } else if (context.kind === 'method') {
+      return function (this: any, ...args: any[]) {
+        const value = target.call(this, ...args)
+        Object.defineProperty(this, context.name, {
+          value:        () => value,
+          writable:     false,
+          enumerable:   true,
+          configurable: true,
+        })
+        return value
+      }
+    } else {
+      throw new Error('@memoized can only be applied to methods or getters')
     }
-  } else if (context.kind === 'method') {
-    return function (this: any, ...args: any[]) {
-      const value = target.call(this, ...args)
-      Object.defineProperty(this, context.name, {
-        value:        () => value,
-        writable:     false,
-        enumerable:   true,
-        configurable: true,
-      })
-      return value
+  }
+  
+  // Experimental decorator syntax (context is propertyKey, descriptor is third arg)
+  if (typeof context === 'string' || typeof context === 'symbol') {
+    const propertyKey = context
+    
+    if (descriptor == null) {
+      throw new Error('@memoized requires a property descriptor')
     }
-  } else {
+    
+    // It's a getter
+    if (descriptor.get != null) {
+      const originalGetter = descriptor.get
+      descriptor.get = function (this: any) {
+        const value = originalGetter.call(this)
+        Object.defineProperty(this, propertyKey, {
+          value,
+          writable:     false,
+          enumerable:   descriptor.enumerable,
+          configurable: descriptor.configurable,
+        })
+        return value
+      }
+      return descriptor
+    }
+    
+    // It's a method
+    if (descriptor.value != null && typeof descriptor.value === 'function') {
+      const originalMethod = descriptor.value
+      descriptor.value = function (this: any, ...args: any[]) {
+        const value = originalMethod.call(this, ...args)
+        Object.defineProperty(this, propertyKey, {
+          value:        () => value,
+          writable:     false,
+          enumerable:   descriptor.enumerable,
+          configurable: descriptor.configurable,
+        })
+        return value
+      }
+      return descriptor
+    }
+    
     throw new Error('@memoized can only be applied to methods or getters')
   }
-}
-
-function memoizedProperty(propertyKey: string, descriptor: PropertyDescriptor, getter: () => any) {
-  descriptor.get = function (this: any) {
-    // Get the value from the getter the first time.
-    const value = getter.call(this)
-
-    // Redefine the property with this cached value.
-    Object.defineProperty(this, propertyKey, {
-      value,
-      writable:     false,
-      enumerable:   descriptor.enumerable,
-      configurable: descriptor.configurable,
-    })
-
-    return value
-  }
-  return descriptor
-}
-
-function memoizedMethod(propertyKey: string, descriptor: PropertyDescriptor, fn: () => any) {
-  descriptor.value = function (this: any) {
-    // Get the value from the getter the first time.
-    const value = fn.call(this)
-
-    // Redefine the property with a simple function returning this cached value.
-    Object.defineProperty(this, propertyKey, {
-      value:        () => value,
-      writable:     false,
-      enumerable:   descriptor.enumerable,
-      configurable: descriptor.configurable,
-    })
-
-    return value
-  }
-  return descriptor
+  
+  throw new Error('@memoized decorator called with invalid arguments')
 }
--- src/decorators/properties.ts
+++ src/decorators/properties.ts
@@ -1,14 +1,55 @@
+/**
+ * Sets the enumerable property of a class member.
+ * Supports both experimental and modern decorator syntax.
+ */
 export function enumerable(flag: boolean = true) {
-  return function <T> (target: any, context: ClassMethodDecoratorContext | ClassGetterDecoratorContext | ClassSetterDecoratorContext) {
-    context.addInitializer(function () {
-      const descriptor = Object.getOwnPropertyDescriptor(this, context.name)
-      if (descriptor) {
-        Object.defineProperty(this, context.name, {
-          ...descriptor,
-          enumerable: flag,
-        })
-      }
-    })
-    return target
+  // Overload signatures for modern decorators
+  function decorator<T, V>(
+    target: (this: T, ...args: any[]) => V,
+    context: ClassMethodDecoratorContext<T, (this: T, ...args: any[]) => V>
+  ): (this: T, ...args: any[]) => V
+  
+  function decorator<T, V>(
+    target: (this: T) => V,
+    context: ClassGetterDecoratorContext<T, V>
+  ): (this: T) => V
+  
+  function decorator<T, V>(
+    target: (this: T, value: V) => void,
+    context: ClassSetterDecoratorContext<T, V>
+  ): (this: T, value: V) => void
+  
+  // Overload signature for experimental decorators
+  function decorator<T>(
+    target: any,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+  ): PropertyDescriptor
+  
+  // Implementation
+  function decorator(target: any, context?: any, descriptor?: PropertyDescriptor): any {
+    // Modern decorator syntax (context is an object with 'kind' property)
+    if (context != null && typeof context === 'object' && 'kind' in context) {
+      context.addInitializer(function (this: any) {
+        const descriptor = Object.getOwnPropertyDescriptor(this, context.name)
+        if (descriptor) {
+          Object.defineProperty(this, context.name, {
+            ...descriptor,
+            enumerable: flag,
+          })
+        }
+      })
+      return target
+    }
+    
+    // Experimental decorator syntax (context is propertyKey, descriptor is third arg)
+    if ((typeof context === 'string' || typeof context === 'symbol') && descriptor != null) {
+      descriptor.enumerable = flag
+      return descriptor
+    }
+    
+    throw new Error('@enumerable decorator called with invalid arguments')
   }
+  
+  return decorator
 }
