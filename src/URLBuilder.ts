d7bbc9eb761c532fe55d3d456519af56eb56a072
Joost Lubach <joostlubach@gmail.com>
2026-06-06 13:50:43 +0200

    Upgrade

--- src/URLBuilder.ts
+++ src/URLBuilder.ts
@@ -1,11 +1,23 @@
-export type URLBuilder = (path?: string) => string
+import { objectEntries } from './lodashext'
+import { Primitive } from './types'
+
+export type URLBuilder = (path?: string, params?: URLSearchParams | Record<string, Primitive>) => string
 
 export namespace URLBuilder {
-  export function create(base: URL): URLBuilder {
-    return path => {
-      const url = new URL(base.href)
+  export function create(base: URL | string): URLBuilder {
+    return (path, params) => {
+      const url = new URL(base instanceof URL ? base.href : base)
       if (path != null) {
-        url.pathname = `${base.pathname.replace(/\/+$/, '')}/${path.replace(/^\/+/, '')}`
+        url.pathname = `${url.pathname.replace(/\/+$/, '')}/${path.replace(/^\/+/, '')}`
+      }
+      if (params instanceof URLSearchParams) {
+        for (const [key, value] of params.entries()) {
+          url.searchParams.append(key, value)
+        }
+      } else if (params != null) {
+        for (const [key, value] of objectEntries(params)) {
+          url.searchParams.append(key, value === true ? '1' : `${value}`)
+        }
       }
       return url.href.replace(/\/+$/, '')
     }
